65bbbf75720f42cc8e1645f39cb242fa3eb8ba88
micheleantonazzi <micheleantonazzi@gmail.com>
2019-05-10 19:00:25 +0200

    added HouseAndStatistics

---
+++ src/main/java/utility/HousesAndStatistics.java
@@ -0,0 +1,42 @@
+package utility;
+
+import messages.HouseMsgs.HouseInfoMsg;
+import messages.StatisticMsgOuterClass.StatisticMsg;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class HousesAndStatistics {
+
+    private static HousesAndStatistics instance;
+
+    private HashMap<HouseInfoMsg, ArrayList<StatisticMsg>> housesMap = new HashMap<>();
+
+    public synchronized static HousesAndStatistics getInstance(){
+        if (instance == null)
+            instance = new HousesAndStatistics();
+        return instance;
+    }
+
+    private HousesAndStatistics(){}
+
+    public synchronized void setHouses(List<HouseInfoMsg> houses){
+        this.housesMap = new HashMap<>();
+        for (HouseInfoMsg house : houses)
+            housesMap.put(house, new ArrayList<>());
+    }
+
+    public synchronized Set<HouseInfoMsg> getHouses(){
+        return this.housesMap.keySet();
+    }
+
+    public synchronized void addHouse(HouseInfoMsg house){
+        this.housesMap.put(house, new ArrayList<>());
+    }
+
+    public synchronized void removeHouse(HouseInfoMsg house){
+        this.housesMap.remove(house);
+    }
+}
